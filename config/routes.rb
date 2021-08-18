fceee36515d39f8c548735c61e06995a962dcd12
shihoono <shiho1449@gmail.com>
2021-08-18 11:08:23 +0900

    devise基本操作

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+  devise_scope :user do
+    root "users/sessions#new"
+  end
+
+  devise_for :users, :controllers => {
+    sessions: 'users/sessions'
+  }
+
+  get 'reports/index'
+  get 'reports/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
